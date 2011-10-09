2eeffbc835876c7261962d3446abebfa89e7ab80
Holger Schmidt <leroyz.mailaccount@gmx.net>
2011-10-09 16:09:33 +0200

    created console application project as CLI to MountFTP

---
+++ Forge.MountFTP.CommandLine/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Args;
+
+namespace Forge.MountFTP.CommandLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var drive = new Drive(Configuration.Configure<Options>().CreateAndBind(args));
+            drive.FtpClientMethodCall += new LogEventHandler(OnFtpClientMethodCall);
+            drive.FtpClientDebug += new LogEventHandler(OnFtpClientDebug);
+
+            Console.WriteLine(drive.Mount());
+            Console.ReadKey();
+        }
+
+        static void WriteColoredLine(string message, ConsoleColor foreGroundColor)
+        {
+            Console.ForegroundColor = foreGroundColor;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+
+        static void OnFtpClientMethodCall(object sender, LogEventArgs args)
+        {
+            WriteColoredLine(args.Message, ConsoleColor.Yellow);
+        }
+
+        static void OnFtpClientDebug(object sender, LogEventArgs args)
+        {
+            WriteColoredLine(args.Message, ConsoleColor.DarkYellow);
+        }
+    }
+}
---
+++ Forge.MountFTP.CommandLine/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Forge.MountFTP.CommandLine")]
+[assembly: AssemblyDescription("The MountFTP command line interface.")]
+[assembly: ComVisible(false)]
+[assembly: Guid("5a74cf61-678c-4515-a542-57bf7a7a1737")]
