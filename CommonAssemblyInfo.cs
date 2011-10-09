0ac37dc2269b7639c7221a7cca13202653689199
Holger Schmidt <leroyz.mailaccount@gmx.net>
2011-10-09 15:21:07 +0200

    created new project + solution Forge.MountFTP

---
+++ CommonAssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+
+[assembly: AssemblyCompany("The Forge")]
+[assembly: AssemblyProduct("MountFTP")]
+[assembly: AssemblyCopyright("Copyright © Holger Schmidt 2011")]
+[assembly: AssemblyTrademark("MountFTP(TM)")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("0.1.*")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
---
+++ Forge.MountFTP/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Forge.MountFTP")]
+[assembly: AssemblyDescription("An FTP client that allows you to mount an FTP server as a virtual drive.")]
+[assembly: ComVisible(false)]
+[assembly: Guid("ef22f07d-c138-4679-930c-a3a86e444c38")]
